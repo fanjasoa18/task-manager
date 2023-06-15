a21406db44a26a17285537541c9d1925121b2fdb
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-06-15 11:53:30 +0300

    feat: store zustand

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,45 @@
-const useTaskManager = () => {
 
+import { create } from "zustand";
+
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean
 }
+
+type TaskState = {
+  tasks: Task[];
+  searchTask: string;
+  addTask: (newTask: Task) => void;
+  updateTask: (id: number, updatedTask: Task) => void;
+  deleteTask: (id: number) => void;
+  setSearchTask: (searchTerm: string) => void;
+};
+
+const useTaskManager = create<TaskState>((set, get) => ({
+  tasks: [],
+  searchTask: '',
+  addTask: (newTask) => {
+    set((state) => ({ tasks: [...state.tasks, newTask] }));
+  },
+  updateTask: (id, updatedTask) => {
+    set((state) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === id ? { ...task, ...updatedTask } : task
+      ),
+    }));
+  },
+  deleteTask: (id) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== id),
+    }));
+  },
+  setSearchTask: (searchTerm) => {
+    set({ searchTask: searchTerm });
+  },
+}));
+
 
 export {
   useTaskManager
-}
+};
